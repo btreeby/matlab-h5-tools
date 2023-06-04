98c4280202bc24e1425140b2c2632efe2c90ef9c
Bradley Treeby <b.treeby@ucl.ac.uk>
2023-06-04 20:24:31 +0100

    Add docs for getSourceRoot

--- tests/getSourceRoot.m
+++ tests/getSourceRoot.m
@@ -1,3 +1,14 @@
-function srcRoot = getSourceRoot
+function srcRoot = getSourceRoot()
+%GETSOURCEROOT Return root folder for toolbox.
+%
+% DESCRIPTION:
+%     getSourceRoot returns the full path to the root folder for the
+%     matlab-h5-tools toolbox.
+%
+% USAGE:
+%     srcRoot = getSourceRoot()
+%
+% OUTPUTS:
+%     srcRoot - Root folder.
+
 srcRoot = fileparts(fileparts(mfilename("fullpath")));
-end
